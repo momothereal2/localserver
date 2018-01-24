a2484bac136a59f60f79bad1a885182e4436f1c7
momothereal2 <glowstone@momoperes.ca>
2018-01-23 19:18:58 -0500

    Add flush

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-console.log("Starting server");
+log("Starting server");
 require('http').createServer((req, res) => {
     res.end("Hello from local server");
 }).listen(8081);
+
+function log(log) {
+    console.log(log);
+    console.log("");
+}
 
